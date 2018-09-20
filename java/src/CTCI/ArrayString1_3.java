7fb2cc94b2b696d63d6fb8bd1d737e24ade9cfae
Tilak <dhanvantari.tilak@sap.com>
2018-09-20 10:18:26 -0700

    ArraysStrings1_3

---
+++ java/src/CTCI/ArrayString1_3.java
@@ -0,0 +1,13 @@
+package CTCI;
+
+import java.io.IOException;
+
+public class ArrayString1_3 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
