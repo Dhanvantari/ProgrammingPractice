be41374546a11f06060b106816dffaa2727e7bd6
Tilak <dhanvantari.tilak@sap.com>
2018-09-20 20:48:29 -0700

    ArrayString1_3

--- java/src/CTCI/ArrayString1_3.java
+++ java/src/CTCI/ArrayString1_3.java
@@ -5,9 +5,33 @@ import java.io.IOException;
 public class ArrayString1_3 {
 
 	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-
+		char[] strArr = {'a', 'b', ' ','c' ,' ', ' '};
+		System.out.println("Before URLify "+ new String(strArr));
+		urlify(strArr, 4);
+		System.out.println("After URLify 1: "+ new String(strArr));
 	}
+
+	private static void urlify(char[] strArr, int originalLen) {
+		int spaceCount=0;
+		for(int i=0; i<originalLen;i++) {
+			if(strArr[i]==' ')
+				spaceCount++;
+		}
+		
+		int newIndex = originalLen + (2*spaceCount)-1;
+		for(int i= originalLen-1; i>=0;i--) {
+			if(strArr[i] == ' ') {
+				strArr[newIndex--] = '0';
+				strArr[newIndex--] = '2';
+				strArr[newIndex--] = '%';
+			}
+			else {
+				strArr[newIndex--] = strArr[i];
+			}
+		}
+		
+		System.out.println("After URLify 2: "+ new String(strArr));
+	}
+	
 
 }
